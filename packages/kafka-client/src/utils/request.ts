dad092f69e3300f999e3e769fdd509644b2ae50b
sincerelyyyash <yta0088@gmail.com>
2025-09-07 23:29:29 +0530

    kafka client changes

--- packages/kafka-client/src/utils/request.ts
+++ packages/kafka-client/src/utils/request.ts
@@ -1,19 +1,19 @@
 
-type RequestType = {
+export type RequestTypes = {
   service: string,
   action: string,
   data: any,
   message: string
 }
 
-export class Request<T> {
+export class KafkaRequest<T> {
   public service: string;
   public action: string;
   public message: string;
   public data: T;
 
 
-  constructor({ service, data, message, action }: RequestType) {
+  constructor({ service, data, message, action }: RequestTypes) {
     this.service = service;
     this.action = action;
     this.data = data;
