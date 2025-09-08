a95664273c78cc78a8abee75ce93af826b67eebe
sincerelyyyash <yta0088@gmail.com>
2025-09-08 18:30:39 +0530

    Prisma migration added for snapshot

---
+++ packages/database/prisma/migrations/20250908114003_snapshot_schema_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Snapshot" (
+    "id" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Snapshot_pkey" PRIMARY KEY ("id")
+);
